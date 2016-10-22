d93219dacfd726b5d1dfc5128784de57d791e0d7
yren <yufei.ren@thomsonreuters.com>
2016-10-22 23:22:33 +0800

    minor change

--- init_box.sh
+++ init_box.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -x
 
+echo "add ~/.screenrc"
+rm -rf ~/.screenrc
+echo "#set this up for convenience" >> ~/.screenrc
+echo "defscrollback 8000" >> ~/.screenrc
+echo "hardstatus alwayslastline \"%Lw\"" >> ~/.screenrc
+echo "hardstatus string '%{= kG}[ %{G}%H %{g}][%= %{= kw}%?%-Lw%?%{r}(%{W}%n*%f%t%?(%u)%?%{r})%{w}%?%+Lw%?%?%= %{g}][%{B} %d/%m %{W}%c %{g}]'" >> ~/.screenrc
+
 echo "install docker"
 sudo yum update -y
 sudo yum install -y docker
