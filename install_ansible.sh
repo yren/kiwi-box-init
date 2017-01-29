4bdd3cfc5a18933543ea9eb1c28d593dcdc09c1c
Yufei Ren <yufei.ren@thomsonreuters.com>
2017-01-29 22:55:58 +0800

    minor change

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
 
-echo "update pip version"
-sudo pip install --upgrade pip
+#echo "update pip version"
+#sudo pip install --upgrade pip
 
-echo "install ansible"
-sudo pip install ansible
+echo "install ansible 2.2"
+sudo pip install ansible==2.2.0.0
 
 echo "create logs dir"
-sudo mkdir -p /rt/logs/ansible 
-sudo chmod 777 /rt/logs/ansible
+sudo mkdir -p /rt/logs/ansible
+sudo chmod 664 /rt/logs/ansible
