5352e02d158a1640c21b946a843e834049bd5a7b
Yufei Ren <yufei.ren@thomsonreuters.com>
2017-06-29 13:37:48 +0800

    minor change

--- install_ghost.sh
+++ install_ghost.sh
@@ -3,8 +3,14 @@
 ### Date: 2017.06.29
 ### Desc: install ghost blog, https://ghost.org/ , https://hub.docker.com/_/ghost/
 
+## exit when error 
+set -e
+
 echo "install docker ghost blog"
-sudo mkdir -p /rt/ghost 
-sudo chmod -R 777 /rt/ghost
+if [[ !(-e /rt/host) ]]; then
+  echo "create dir /rt/ghost"
+  sudo mkdir -p /rt/ghost 
+  sudo chmod -R 777 /rt/ghost
+fi
 
 docker run -d -p 80:2368 --name kiwi-ghost -v /rt/ghost:/var/lib/ghost ghost:0.11.10
