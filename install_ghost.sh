e1598cb96b05f404a03e3e41b00dd9f0ab2225c2
Yufei <yufei.ren@thomsonreuters.com>
2018-01-02 01:19:41 +0800

    Update install_ghost.sh

--- install_ghost.sh
+++ install_ghost.sh
@@ -13,4 +13,4 @@ if [[ !(-e /rt/ghost) ]]; then
   sudo chmod -R 777 /rt/ghost
 fi
 
-docker run -d -p 80:2368 --name kiwi-ghost -v /rt/ghost:/var/lib/ghost ghost:0.19.0
+docker run -d -p 80:2368 --name kiwi-ghost -v /rt/ghost:/var/lib/ghost/content ghost:1.19.0
